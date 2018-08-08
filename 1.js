ba12a46c988d89ec3202aebbfe0bc6dbf3e4763b
nli2 <a-nli2@expedia.com>
2018-08-08 15:33:20 -0700

    initial commit

---
+++ 1.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb').MongoClient;
+let db = null;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+mongodb.connect('mongodb://localhost:27017').then(x => db = x.db('ygd'));
+
+app.get('/', (req, res) => res.json('Hello World'));
+
+app.get('/api/post/:id', (req, res) =>
+  db.collection('posts')
+    .findOne({ id: +req.params.id })
+    .then(r => res.json(r))
+);
+
+app.post('/api/post', (req, res) =>
+  db.collection('posts')
+    .insert(req.body)
+    .then(r => res.send('done'))
+);
+
+app.listen('8001');
---
+++ 2.js
@@ -0,0 +1,4 @@
+const add = require('./1');
+
+console.log(add(3, 6));
+
